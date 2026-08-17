d46c7f02ecec4c58785f4b4ce5b12be50412c995
furrypython <furrypythonfollies@gmail.com>
2026-08-17 18:32:23 +0900

    Delete test files

--- test_nan_prop.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let max1 = f64::max(std::f64::NAN, 1.0);
-    let max2 = f64::max(1.0, std::f64::NAN);
-    println!("max(NaN, 1.0) = {}", max1);
-    println!("max(1.0, NaN) = {}", max2);
-}
